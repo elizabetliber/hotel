e54f30b0d110f469ee6311185813e67fd5a928e3
Elizabet Liber <elizabet.liber@gmail.com>
2019-06-18 17:53:33 +0500

    я добавила header,footer,div выровняли текст по центру и по вертикали добавили свойства

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p className="description">
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+
+            <header className="header">
+                <h1>Guests house coral</h1>
             </header>
+
+            <div>
+                <div>
+                    <p>Уютный семейный номер</p>
+                </div>
+            </div>
+
+            <footer className="footer">
+                <h3>Guests house coral</h3>
+            </footer>
+
         </div>
     );
 }
