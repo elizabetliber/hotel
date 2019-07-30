819ca13945a6831c564cb48ef654cd689eb2e296
Elizabet Liber <elizabet.liber@gmail.com>
2019-07-30 15:22:21 +0500

    обновление

--- src/App.js
+++ src/App.js
@@ -5,32 +5,59 @@ function App() {
     return (
         <div className="App">
 
-            <div className="header">
-                <h1>Guests house coral</h1>
+            <div className="mainImageContainer">
+                <img className="mainImage" src="https://rosting.by/upload/iblock/b8d/109518088.jpg" />
+                <div className="titleContainer">
+                    <h1 className="title">Guests house coral</h1>
+                </div>
             </div>
+            <div className="rambo">
+                <div className="container">
+                    <div className="wrapper">
+                        <div className="element">
+                            <div className="you">
+                                <p>номер целиком    <strong>1 кровать</strong></p>
+                            <h4>Уютный семейный номер</h4>
+                                <p>1133ք</p>
+                                <p>2 отзыва</p>
+                            </div>
+                        </div>
+                        <div className="element">
 
-            <div className="container">
-                <div className="wrapper">
-                    <div className="element">
-                        <h4>Уютный семейный номер</h4>
-                    </div>
-                    <div className="element">
-                        <h4>Уютный семейный номер</h4>
-                    </div>
-                    <div className="element">
-                        <h4>Уютный семейный номер</h4>
-                    </div>
-                    <div className="element">
-                        <h4>Уютный семейный номер</h4>
+                            <div className="you">
+                                <p>номер целиком    <strong>1 кровать</strong></p>
+                                <h4>Уютный семейный номер</h4>
+                                <p>1133ք</p>
+                                <p>2 отзыва</p>
+                            </div>
+                        </div>
+                        <div className="element">
+                            <div className="you">
+                                <p>номер целиком    <strong>1 кровать</strong></p>
+                                <h4>Уютный семейный номер</h4>
+                                <p>1133ք</p>
+                                <p>2 отзыва</p>
+                            </div>
+                        </div>
+                        <div className="element">
+                            <div className="you">
+                                <p>номер целиком    <strong>1 кровать</strong></p>
+                                <h4>Уютный семейный номер</h4>
+                                <p>1133ք</p>
+                                <p>2 отзыва</p>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="footer">
-                <h3>Guests house coral</h3>
-                <p>г.Анапа, Коралловый проезд, д.19</p>
-                <p>+ 7(996)376-19-78</p>
+            <div className="box">
+                <div className="footer">
+                    <h3>GUEST HOUSE CORAL</h3>
+                    <p>г.Анапа, Коралловый проезд, д.19</p>
+                    <p>+ 7(996)376-19-78</p>
+                </div>
             </div>
-
         </div>
     );
 }
